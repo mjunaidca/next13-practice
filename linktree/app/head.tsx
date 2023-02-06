1f646f299da4706ce083c11ae00f3db9c1163ff4
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-02-06 05:09:29 +0500

    Link tree

---
+++ linktree/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Link Tree</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Linktree using nextjs 13 and tailwind css" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
