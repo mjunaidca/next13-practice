3209ad00582f803f3dfefbeb2b77e5d135e62e4c
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-02-08 20:52:40 +0500

    Creatin panaverse practice site with Jahanzaib

---
+++ tailwind-book/app/01/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div className="flex">
+    <div>
+    <img src="./media/music.svg" width="100" height="100" />
+    </div>
+    <div>
+    <h1>Welcome to NorthBy</h1>
+    <h2>A premium in sight and sound</h2>
+    <button>Learn More</button>
+    </div>
+    </div>
+  )
+}
--- tailwind-book/app/page.tsx
+++ tailwind-book/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      hi
+      <h1>ol</h1>
     </>
   )
 }
