675580e590cb98ca915ac9afc6e19114b53ea73c
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-02-11 03:04:04 +0500

    Incomplete

--- panaverse-web/app/page.tsx
+++ panaverse-web/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
    <main>
-    Hi
+    Home
     </main>
   )
 }
--- panaverse-web/views/navbar/index.tsx
+++ panaverse-web/views/navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Flex, Text, Link, Stack } from "@chakra-ui/react";
+import { Box, Flex, Text, Link, Stack, Button } from "@chakra-ui/react";
 import Image from "next/image";
 import NextLink from "next/link";
 
@@ -14,34 +14,62 @@ const Navbar = () => {
         borderStyle="solid"
         borderColor={"gray.200"}
         align="center"
-        px={{base:2}}
-        py={{base:2}}
-        >
-        <Flex flex={{ base: 1 }} justify={{ base:"center", md: "start" }}>
+        px={{ base: 2 }}
+        py={{ base: 2 }}
+
+      >
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }} >
           <Link as={NextLink} href='/'>
             <Image
-            src="/p.png"
-            alt="logo"
-            width={'150'}
-            height='50' />
+              src="/p.png"
+              alt="logo"
+              width={'150'}
+              height='50' />
           </Link>
           <Flex
-          display = {{base: "none", md: "flex"}}
-          ml={10}
-          alignItems="center"
+            display={{ base: "none", md: "flex" }}
+            ml={10}
+            alignItems="center"
+            flex={1}
           >
             <Text px={2} >A</Text>
             <Text px={2}>B</Text>
             <Text px={2}>C</Text>
             <Text px={2}>D</Text>
           </Flex>
-          <Stack 
-          flex= {{ base:0}}
-          justify={"flex-end"}
-          direction={"row"}
-          spacing={6}
+          <Stack
+            flex={{ base: 0 }}
+            justify={"flex-end"}
+            direction={"row"}
+            spacing={6}
           >
-                asdf
+            <Link 
+            as={NextLink} 
+            isExternal
+            href='github.com/panaverse'
+            margin={"auto"}
+            display={{ base: "none", md: "inline-flex"}}
+            >
+              <Button
+                fontWeight={400}
+                colorScheme="messenger"
+                >Github 
+                </Button>
+            </Link>
+
+            <Link 
+            as={NextLink} 
+            isExternal
+            href='piaic.org'
+            >
+              <Button
+                fontWeight={400}
+                variant="link"
+                color={"blue"}
+                >Apply Now 
+                </Button>
+            </Link>
+
           </Stack>
         </Flex>
       </Flex>
